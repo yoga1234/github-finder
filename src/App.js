291520e08249a61674376e13d2cc21cc5c6bc2fc
Septiana Yoga <septianayoga30@gmail.com>
2022-02-13 20:32:24 +0700

    adding clear button for the form

--- src/App.js
+++ src/App.js
@@ -21,12 +21,20 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
