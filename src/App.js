5b8c23691cfb6f4208563c1e8297dcd65eebe1b5
Septiana Yoga <septianayoga30@gmail.com>
2022-02-14 14:20:58 +0700

    adding routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -59,7 +60,8 @@ class App extends Component {
                     />
                   </Fragment>
                 )}
-              ></Route>
+              />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
         </div>
